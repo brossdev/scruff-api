de17b8256593cac75cc3cf99e1c9d2d4bca4aa18
Bobby <Bobby1985uk@gmail.com>
2020-04-12 21:01:15 +0100

    chore: add offline and jest config for dynamo db

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,6 +1,23 @@
 import AWS from "aws-sdk";
 
-const client = new AWS.DynamoDB.DocumentClient();
+let config = {};
+if (process.env.IS_OFFLINE) {
+  config = {
+    endpoint: "localhost:8000",
+    region: "localhose",
+  };
+}
+
+if (process.env.JEST_WORKER_ID) {
+  config = {
+    convertEmptyValues: true,
+    endpoint: "localhost:8000",
+    sslEnabled: false,
+    region: "local-env",
+  };
+}
+
+const client = new AWS.DynamoDB.DocumentClient(config);
 
 export default {
   get: (params) => client.get(params).promise(),
