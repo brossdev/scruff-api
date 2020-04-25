ca8dd3caf0dd1de08a09129b09156bd0c0bbbdca
Bobby <Bobby1985uk@gmail.com>
2020-04-25 12:03:55 +0100

    chore: add testing config

--- services/customers/api.ts
+++ services/customers/api.ts
@@ -1,18 +1,31 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${await message({
-        time: 1,
-        copy: "Your function executed successfully!",
-      })}`,
-    }),
-  };
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello from Apollo',
+  },
 };
 
-const message = ({ time, ...rest }) =>
-  new Promise((resolve, reject) =>
-    setTimeout(() => {
-      resolve(`${rest.copy} (with a delay)`);
-    }, time * 1000)
-  );
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+});
+
+export const handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
